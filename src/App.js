2a9edbad78b66c00e428bc3064b40420894931d7
minh57 <viminh999999@gmail.com>
2022-06-23 20:22:33 +0700

    btvn

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import {Card} from "./components/card"
+import Header from './components/header';
+import Wrapper from './components/wrapper';
+function App() {
+  return (
+    <div className="Container">
+      <div>
+            Unique travel experiences
+    </div>
+    <div>
+      From renovation and room addition to masonary and other handy man services
+    </div>
+     
+      <div>
+        <Wrapper/>
+        </div>
+      <div>
+        <Wrapper/>
+        </div>
+        
+
+     
+      </div>
+      
+      
+      
+  );
+}
+
+export default App;
+
+    
---
+++ src/components/wrapper.jsx
@@ -0,0 +1,11 @@
+import Item from "./item"
+export default function Wrapper (){
+    return (
+      <div className="Contain">
+       <Item> </Item>
+       <Item></Item>
+       <Item></Item>
+      </div>
+    )
+}
+      
